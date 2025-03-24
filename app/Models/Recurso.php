c8018529fbd30337939243f30f3415c50a784316
ShadowKnSs <xavirohe123@hotmail.com>
2025-03-24 13:10:01 -0600

    Cambios en el balde

--- app/Models/Recurso.php
+++ app/Models/Recurso.php
@@ -2,23 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Recurso extends Model
 {
-
-    protected $table = 'recursos';
-    protected $primaryKey = 'idRecursos';
-    // Campos asignables masivamente
-    protected $fillable = [
-        'idProyectoMejora',
-        'descripcionRec',
-        'recursosMatHum',
-        'costo',
-    ];
-
-    // Deshabilitar timestamps si la tabla no tiene created_at y updated_at
-    public $timestamps = false;
-
+    //
 }
