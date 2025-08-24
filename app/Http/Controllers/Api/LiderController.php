b2f336dfae0593eac47ff026e9b0be810533db58
Pgrg <palmirag211@gmail.com>
2025-08-24 10:49:55 -0600

    cambios raros

--- app/Http/Controllers/Api/LiderController.php
+++ app/Http/Controllers/Api/LiderController.php
@@ -8,9 +8,21 @@ use App\Models\Usuario;
 
 class LiderController extends Controller
 {
-    public function index() {
+    /*public function index() {
         $leaders = Usuario::where('idTipoUsuario', 4)->get();
     
+        return response()->json(['leaders' => $leaders], 200);
+    }*/
+    public function index()
+    {
+        // Requiere que en el modelo Usuario exista:
+        // public function roles() { return $this->belongsToMany(TipoUsuario::class, 'usuario_tipo', 'idUsuario', 'idTipoUsuario'); }
+        $leaders = Usuario::with(['roles'])   // opcional: para ver sus roles
+            ->whereHas('roles', function ($q) {
+                $q->where('nombreRol', 'Líder');      // o ->where('idTipoUsuario', 2) si prefieres por id
+            })
+            ->get();
+
         return response()->json(['leaders' => $leaders], 200);
     }
     
